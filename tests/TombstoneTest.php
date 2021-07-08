223bd5d707cdaf99eec50d846bed878066ea0f0c
Sarel van der Walt <sarel@afrihost.com>
2021-07-08 23:38:39 +0200

    singleton testing

--- tests/TombstoneTest.php
+++ tests/TombstoneTest.php
@@ -1,10 +1,23 @@
 <?php
 
-
 use SarelvdWalt\Tombstone\Tombstone;
 use PHPUnit\Framework\TestCase;
+use SarelvdWalt\Tombstone\TombstoneBase;
 
 class TombstoneTest extends TestCase
 {
+    public function testInstantiateThroughSingleton()
+    {
+        $ts = Tombstone::getInstance();
 
+        $this->assertInstanceOf(TombstoneBase::class, $ts);
+    }
+
+    public function testOnlyGetOneInstanceAfterMultipeSingletonCalls()
+    {
+        $ts = Tombstone::getInstance();
+        $ts2 = Tombstone::getInstance();
+
+        $this->assertSame($ts, $ts2);
+    }
 }
