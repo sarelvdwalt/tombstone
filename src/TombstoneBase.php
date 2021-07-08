ff5b85f601b07405c18a03442c8e189920e9b4e4
Sarel van der Walt <sarel@afrihost.com>
2021-07-08 23:38:13 +0200

    singleton implementation of tombstone

--- src/TombstoneBase.php
+++ src/TombstoneBase.php
@@ -1,7 +1,33 @@
 <?php
+
 namespace SarelvdWalt\Tombstone;
 
 abstract class TombstoneBase
 {
+    private static Tombstone $singleton;
+
+    /**
+     * Tombstone constructor: Deliberately made protected to avoid misuse.
+     */
+    protected function __construct()
+    {
+    }
+
+    /** Avoid cloning (only use singleton to instantiate) */
+    protected function __clone()
+    {
+    }
+
+    /**
+     * @return Tombstone
+     */
+    public static function getInstance(): Tombstone
+    {
+        if (!isset(self::$singleton)) {
+            self::$singleton = new static();
+        }
+
+        return self::$singleton;
+    }
 
 }
