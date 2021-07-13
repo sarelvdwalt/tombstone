13f85b77a4df69389ae273809d660f85c970fee9
Sarel van der Walt <sarel@afrihost.com>
2021-07-13 09:12:42 +0200

    record tombstone calls

--- src/TombstoneBase.php
+++ src/TombstoneBase.php
@@ -6,6 +6,8 @@ abstract class TombstoneBase
 {
     private static Tombstone $singleton;
 
+    protected array $records;
+
     /**
      * Tombstone constructor: Deliberately made protected to avoid misuse.
      */
@@ -31,6 +33,25 @@ abstract class TombstoneBase
         return self::$singleton;
     }
 
+    public function record($uniqueIdentifier)
+    {
+        $timer = -microtime(true);
+        if (!array_key_exists($uniqueIdentifier, $this->records)) {
+            $trace = debug_backtrace(false, 1);
+
+            $this->records[$uniqueIdentifier] = [
+                'c' => 0,
+                'first_seen' => microtime(true),
+                'time_taken' => 0,
+                'trace_file' => $trace[0]['file'],
+                'trace_line' => $trace[0]['line'],
+            ];
+        }
+        $timer += microtime(true);
+
+        $this->records[$uniqueIdentifier]['c']++;
+        $this->records[$uniqueIdentifier]['time_taken'] += $timer;
+    }
 
     abstract public function shutdown();
 }
